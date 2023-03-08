37f236baafef616cee1eb87589c7f4c99caa330f
Carlos Mayer <carlitos5525.cap@gmail.com>
2023-03-08 13:49:07 -0300

    Ticket database

--- tickets_app/models.py
+++ tickets_app/models.py
@@ -1,12 +1,17 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class Ticket(models.Model):
     
     is_available = models.BooleanField(default = True)
-    event_name = models.CharField(max_length=255, default="")
-    price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    is_sold = models.BooleanField(default = False, blank=True)
+    event_name = models.CharField(max_length=255, default="", blank=False, null=False)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0, blank=False, null=False)
+    description = models.CharField(max_length=300, default="", blank=True)
+    creation_date = models.DateField(auto_now_add=True)
+    sales_date = models.DateField(blank=True, null=True)
     
     def __str__(self):
-        return self.name
+        return self.event_name
     
